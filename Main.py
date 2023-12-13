5e69292b41c5131ae8a772952acb79c267ceefaf
AxelBianchi <axel.bianchi@esme.fr>
2023-12-13 18:01:59 +0100

    feat get and print message

---
+++ Main.py
@@ -0,0 +1,6 @@
+import base64
+
+def ingest_message(event, context):
+    message = base64.b64decode(event['data']).decode('utf-8') #Récup event sans le reste (event et context)
+    print(message)
+
