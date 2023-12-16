184dc76934226bee6a26fe1f955a9091942af2c1
AxelBianchi <axel.bianchi@esme.fr>
2023-12-16 10:46:47 +0100

    fix two variables on ingest_message to fit with entry PubSub

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from verif_msg import verif_msg
 from message_to_login import message_to_login
 from message_to_login_error import message_to_login_error
 
-def ingest_message(event):
+def ingest_message(event, context):
     message = base64.b64decode(event['data']).decode('utf-8') #Récup event sans le reste (event et context)
     print(message)
 
