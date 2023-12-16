4a4555b295cc17ae2ccfb2d1f02358ee1c5bb27e
AxelBianchi <axel.bianchi@esme.fr>
2023-12-16 14:45:52 +0100

    fix modifying scripts order and names, trying to write on login

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import base64
 import json
-from verif_msg import verif_msg
-from message_to_login import message_to_login
-from message_to_login_error import message_to_login_error
+from messageProcessing import verif_msg
+from tables_sender import message_to_login
+
+codes = {
+    1: "error_client_id",
+    2: "error_timestamp",
+    3: "normal_login"
+}
 
 def ingest_message(event, context):
     message = base64.b64decode(event['data']).decode('utf-8') #Récup event sans le reste (event et context)
     print(message)
 
     message_json = json.load(event['data'])
-    error_message = verif_msg(message_json)
-    if error_message is None:
-        print("fonction normale")
-        message_to_login(message_json) #Ecrire dans la table normale
-    if error_message is not None:
-        print("non  ")
-        message_to_login_error(message_json)
+    error_code = verif_msg(message_json)
+    error_message = codes[error_code]
+
+    if error_code == 3:
+        message_to_login(message_json)
--- messageProcessing.py
+++ messageProcessing.py
@@ -1,6 +1,7 @@
 import datetime
 from datetime import datetime
 
+
 def verif_msg(message_json):
 
     client_id = message_json.get('client_id')
@@ -8,16 +9,16 @@ def verif_msg(message_json):
 
     #Vérifier l'existence du client_id
     if client_id is None:
-        error_message = "Invalid client_id"
+        error_message = 1
         return error_message
 
     #Vérifier la cohérence du login_timestamp
     current_time = datetime.utcnow().timestamp()
     max_acceptable_timestamp = current_time + 3600  #Marge d'une heure pour éviter les problèmes de synchronisation
     if login_timestamp > max_acceptable_timestamp or login_timestamp < 0:
-        error_message = "Invalid Timestamp"
+        error_message = 2
         return error_message
 
     else:
-        error_message = None
+        error_message = 3
         return error_message
