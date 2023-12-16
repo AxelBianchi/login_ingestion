de302cdc40fd810ca953e96a24a6eb107ea18911
AxelBianchi <axel.bianchi@esme.fr>
2023-12-16 22:05:43 +0100

    fix error_codes

--- messageProcessing.py
+++ messageProcessing.py
@@ -2,9 +2,9 @@ import datetime
 from datetime import datetime
 
 error_codes = {
-    'error_client_id': 1,
-    'error_timestamp': 2,
-    'normal_login': 0
+    "error_client_id": 1,
+    "error_timestamp": 2,
+    "normal_login": 0
 }
 
 def verif_msg(message_json):
@@ -14,19 +14,19 @@ def verif_msg(message_json):
 
     #Vérifier l'existence du client_id
     if client_id is None:
-        return error_codes['error_client_id']
+        return error_codes["error_client_id"]
 
     #Vérifier la cohérence du login_timestamp
     current_time = datetime.utcnow().timestamp()
     if login_timestamp > current_time:
-        return error_codes['error_timestamp']
+        return error_codes["error_timestamp"]
 
     else:
-        return error_codes['normal_login']
+        return error_codes["normal_login"]
 
 
 def get_error_reason(error_code):
     for reason, code in error_codes.items():
         if code == error_code:
             return reason
-    return 'error not found'
+    return "error not found"
--- tables_sender.py
+++ tables_sender.py
@@ -22,7 +22,7 @@ def message_to_login_error(message_json_format,reason):
     client = bigquery.Client()
     table_id = "ethereal-casing-404517.raw_dataset.login_errors"
 
-    message_json_format['reason'] = reason
+    message_json_format["reason"] = reason
     # Insert data into BigQuery
     rows_to_insert = [message_json_format]
 
