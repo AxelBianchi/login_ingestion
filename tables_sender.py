49b93c6f8e1f83dc22530404713c89c66454d7a4
AxelBianchi <axel.bianchi@esme.fr>
2023-12-17 13:43:37 +0100

    fix error_codes

--- tables_sender.py
+++ tables_sender.py
@@ -1,5 +1,5 @@
 from google.cloud import bigquery
-
+import json
 def message_to_login(message_json_format):
     print("GOOD MESSAGE")
 
@@ -18,14 +18,16 @@ def message_to_login(message_json_format):
 
 def message_to_login_error(message_json_format,reason):
     print("BAD MESSAGE ")
+    message_json_str = str(message_json_format).replace("'", '"')
+    message_json = json.loads(message_json_str)
 
     client = bigquery.Client()
     table_id = "ethereal-casing-404517.raw_dataset.login_errors"
 
-    message_json_format["reason"] = reason
-    print(message_json_format)
+    message_json["reason"] = reason
+    print(message_json)
     # Insert data into BigQuery
-    rows_to_insert = [message_json_format]
+    rows_to_insert = [message_json]
 
     errors = client.insert_rows_json(table_id, rows_to_insert)
 
